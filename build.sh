e179230ba6e6a8bcd32619aefc042fb26b8890b3
quiaingmuarua <2424619166@qq.com>
2025-11-18 14:06:49 +0800

    Update plugin path in build.sh script
    Changed references from liblearn_llvm_pass.so to learn_llvm_pass.so in all plugin-related commands to match the correct shared library name. Also removed redundant directory change and improved script consistency.

--- build.sh
+++ build.sh
@@ -4,20 +4,20 @@ export LLVM_DIR=/root/open_source/llvm-build/lib/cmake/llvm
 cmake .
 cmake --build .
 
-
+cd /root/open_source/learn_llvm
 
 /root/open_source/llvm-build/bin/opt \
-  -load-pass-plugin ./lib/liblearn_llvm_pass.so \
+  -load-pass-plugin ./lib/learn_llvm_pass.so \
   -passes=hello-pass,junk-pass \
   -S ./learn-pass/tests/hello_input.ll \
   -o ./tmp/out_both.ll
 
 
-cd /root/open_source/learn_llvm
+
 
 /root/open_source/llvm-build/bin/clang \
   -O1 \
-  -fpass-plugin=./lib/liblearn_llvm_pass.so \
+  -fpass-plugin=./lib/learn_llvm_pass.so \
   -S -emit-llvm \
   ./example/test.c \
   -o ./tmp/add_pass.ll
@@ -26,7 +26,7 @@ cd /root/open_source/learn_llvm
 /root/open_source/llvm-build/bin/clang \
   --target=aarch64-linux-gnu \
   -O1 \
-  -fpass-plugin=./lib/liblearn_llvm_pass.so \
+  -fpass-plugin=./lib/learn_llvm_pass.so \
   -fPIC -shared \
   ./example/test.c \
   -o ./tmp/libexample_arm64.so
@@ -35,7 +35,7 @@ cd /root/open_source/learn_llvm
 /root/open_source/llvm-build/bin/clang \
   --target=aarch64-linux-gnu \
   -O1 \
-  -fpass-plugin=./lib/liblearn_llvm_pass.so \
+  -fpass-plugin=./lib/learn_llvm_pass.so \
   ./example/test.c \
   -o ./tmp/example_arm64
 
@@ -44,6 +44,6 @@ cd /root/open_source/learn_llvm
 /root/open_source/llvm-build/bin/clang \
   --target=x86_64-linux-gnu \
   -O1 \
-  -fpass-plugin=./lib/liblearn_llvm_pass.so \
+  -fpass-plugin=./lib/learn_llvm_pass.so \
   ./example/test.c \
   -o ./tmp/example_x86_64
